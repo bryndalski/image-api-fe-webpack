1b2d7ae17d9e093b25de066da3a1fd4c8751eae5
bryndalski <kubabryndal@gmail.com>
2024-03-30 14:14:47 +0100

    created login page

--- src/login/index.ts
+++ src/login/index.ts
@@ -1,1 +1,14 @@
 import "./scss/style.scss"
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const imageGrid = document.getElementById('image-grid');
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('image-cell');
+        cell.style.backgroundImage = `url('https://picsum.photos/100/100/?random&t=${new Date().getTime()+ Math.random()*1000}')`;
+        imageGrid.appendChild(cell);
+    }
+})
+
+
